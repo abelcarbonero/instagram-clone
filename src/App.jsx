a5eb498fee7c9f47e9b03fcf55a6becbaeb5d26c
Abel Carbonero <79545908+abelcarbonero@users.noreply.github.com>
2022-06-08 19:20:45 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+const App = (
+	<div>
+		<h1>Vite Template</h1>
+	</div>
+);
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import App from './App';
+
+const container = document.getElementById('root');
+
+ReactDOM.render(App, container);
